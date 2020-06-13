b86c0dcda491868304931666371538b2e2790aa3
metafor <silveryflow@naver.com>
2020-06-13 18:50:12 +0900

    쓸대없는 파일 제거

--- src/main.js
+++ src/main.js
@@ -1,7 +1,5 @@
 import Vue from "vue";
 import App from "./App.vue";
-import router from "./router";
-import store from "./store";
 import { library } from "@fortawesome/fontawesome-svg-core";
 import { faPlus } from "@fortawesome/free-solid-svg-icons";
 import { faCheck } from "@fortawesome/free-solid-svg-icons";
@@ -15,7 +13,5 @@ Vue.component("font-awesome-icon", FontAwesomeIcon);
 Vue.config.productionTip = false;
 
 new Vue({
-	router,
-	store,
 	render: (h) => h(App),
 }).$mount("#app");
